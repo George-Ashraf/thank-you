506b295c240ce143ce5ca0f37e8b28fe2d7f2f24
George-Ashraf <georgeid20@gmail.com>
2023-04-28 19:46:53 +0300

    responsive navbar

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 
 let button = document.querySelector(".mode")
 let body = document.querySelector("body")
-let nav = document.querySelector("nav ")
+let nav = document.querySelector("header ")
 let li = document.querySelectorAll("nav  a")
 let head = document.querySelectorAll(".H")
 let table = document.querySelector("table")
+let logo = document.querySelector(".logo a")
+
 
 button.onclick = function () {
     if(body.classList.contains("dark") )
@@ -13,6 +15,7 @@ button.onclick = function () {
         for(let i=0;i<head.length;i++){
             head[i].style.color="darkslategrey"}
             button.textContent="Dark mode"
+            logo.style.color="black"
             nav.style.background="white"
             nav.style.boxShadow="0 10px 60px black"
             for(let i=0;i<li.length;i++)
@@ -28,6 +31,8 @@ button.onclick = function () {
         for(let i=0;i<head.length;i++){
         head[i].style.color="green"}
         button.textContent="Light mode"
+        logo.style.color="white"
+
         nav.style.background="black"
         nav.style.boxShadow="0px 0px 20px white"
         for(let i=0;i<li.length;i++)
@@ -43,3 +48,20 @@ button.onclick = function () {
     }
     
 }    
+// nav
+var toggle =document.querySelector( '.menu-toggle');
+toggle.onclick=function(){
+    var nav=document.querySelector('nav');
+    nav.classList.toggle('active')
+
+}
+var link=document.querySelectorAll('ul li ');
+for(let i=0 ; i<link.length; i++){
+
+link[i].onclick=function(){
+    var nav=document.querySelector('nav');
+    nav.classList.toggle('active')
+    
+}
+
+}
